475d55e01fc302ea400c7b6a63af45e63b5348f6
TejasPatne <tejaspatne18@gmail.com>
2024-01-17 23:56:05 +0530

    feat: add navbar and sidebar

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen md:hidden">
       <UserButton afterSignOutUrl="/"/>
     </div>
   )
