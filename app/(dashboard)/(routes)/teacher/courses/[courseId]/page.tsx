2a82a10f4fabc6349b64a14b9d752cb505426029
TejasPatne <tejaspatne18@gmail.com>
2024-01-23 13:14:20 +0530

    feat: add course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import { IconBadge } from '@/components/icon-badge';
 import { LayoutDashboard } from 'lucide-react';
+import { TitleForm } from './_components/page';
 
 const CourseIdPage = async ({ params } : { params: { courseId: string }}) => {
     const { userId } = auth();
@@ -46,6 +47,7 @@ const CourseIdPage = async ({ params } : { params: { courseId: string }}) => {
                         <IconBadge icon={LayoutDashboard} />
                         <h2 className='text-xl'>Customize your course</h2>
                     </div>
+                    <TitleForm initialData={course} courseId={course.courseId} />
                 </div>
             </div>
         </div>
