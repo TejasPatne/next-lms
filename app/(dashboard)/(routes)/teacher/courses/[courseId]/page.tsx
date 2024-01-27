dbac30fbeff668abeeebfe15d32953c8a061c2eb
TejasPatne <tejaspatne18@gmail.com>
2024-01-27 15:39:03 +0530

    feat: add chapter creation api

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,7 @@ import { ImageForm } from './_components/image-form';
 import { CategoryForm } from './_components/category-form';
 import { PriceForm } from './_components/price-form';
 import { AttachmentForm } from './_components/attachment-form';
+import { ChaptersForm } from './_components/chapters-form';
 
 const CourseIdPage = async ({ params } : { params: { courseId: string }}) => {
     const { userId } = auth();
@@ -18,9 +19,15 @@ const CourseIdPage = async ({ params } : { params: { courseId: string }}) => {
     }
     const course = await db.course.findUnique({
         where: {
-            id: params.courseId
+            id: params.courseId,
+            userId
         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: "asc"
+                }
+            },
             attachments: {
                 orderBy: {
                     createdAt: "desc"
@@ -41,7 +48,8 @@ const CourseIdPage = async ({ params } : { params: { courseId: string }}) => {
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished)
     ];
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
@@ -75,9 +83,7 @@ const CourseIdPage = async ({ params } : { params: { courseId: string }}) => {
                             <IconBadge icon={ListChecks} />
                             <h2 className='text-xl'>Course chapters</h2>
                         </div>
-                        <div>
-                            TODO: chapters
-                        </div>
+                        <ChaptersForm initialData={course} courseId={course.id} />
                     </div>
                     <div>
                         <div className='flex items-center space-x-2'>
